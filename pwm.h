3525bff300ee0714a58dba48002629f37176b0cb
Slamkas <seslamka@yahoo.com>
2014-10-21 13:50:08 -0700

    Update pwm.h

--- pwm.h
+++ pwm.h
@@ -7,3 +7,21 @@
 //  CSE325 Embedded Microprocessor Systems
 //  Fall 2014
 //
+
+typedef enum {
+ clk_src_a  = 0, //
+ clk_src_sa = 1, //
+ clk_src_b  = 2, //
+ clk_src_sb = 3
+} clk_src_t;
+
+typedef enum {
+ pwm_channel_1  = 0, //
+ pwm_channel_2  = 1, //
+ pwm_channel_3  = 2, //
+ pwm_channel_4  = 3, //
+ pwm_channel_5  = 4, //
+ pwm_channel_6  = 5, //
+ pwm_channel_7  = 6, //
+ pwm_channel_8  = 7 //
+} pwm_channel_t;
