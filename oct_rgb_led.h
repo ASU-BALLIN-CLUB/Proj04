56c7f43d13b7f1c899ee98df4b92a619dca766a6
Slamkas <seslamka@yahoo.com>
2014-10-21 14:12:29 -0700

    Update oct_rgb_led.h

--- oct_rgb_led.h
+++ oct_rgb_led.h
@@ -7,3 +7,9 @@
 //  CSE325 Embedded Microprocessor Systems
 //  Fall 2014
 //
+
+typedef enum {
+ LED1_COM = 4, // Pin 4 of a GPIO port
+ LED2_COM = 6, // Pin 6 of a GPIO port
+ LED3_COM = 7, // Pin 7 of a GPIO port
+} oct_rbg_pin_t;
