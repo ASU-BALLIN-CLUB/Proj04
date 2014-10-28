e6659300a1c1753da7e54832f473a2d733770d07
Slamkas <seslamka@yahoo.com>
2014-10-28 14:46:02 -0700

    Update oct_rgb_led.h

--- oct_rgb_led.h
+++ oct_rgb_led.h
@@ -13,3 +13,14 @@ typedef enum {
  LED2_COM = 6, // Pin 6 of a GPIO port
  LED3_COM = 7, // Pin 7 of a GPIO port
 } oct_rbg_pin_t;
+
+typedef enum {
+ LED1_COM1 = 0, // Pin 4 of a GPIO port
+ LED2_COM1 = 1, // Pin 6 of a GPIO port
+ LED3_COM1 = 2, // Pin 7 of a GPIO port
+} oct_rgb_led_t;
+
+typedef enum {
+ false = 0;
+ true = 1;
+} bool_t;
