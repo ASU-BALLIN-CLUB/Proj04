a7397df1ec9c452b90e65f44690ac8bece5ec135
Slamkas <seslamka@yahoo.com>
2014-11-01 13:00:05 -0700

    Create global.c

---
+++ global.c
@@ -0,0 +1,22 @@
+//**********************************************************************************************************************************
+// FILE: global.c
+//
+// DESCRIPTION
+// See comments in global.h.
+//
+// AUTHORS
+// Kevin R. Burger (burgerk@asu.edu)
+// Computer Science & Engineering
+// Arizona State University, Tempe, AZ 85287-8809
+// http://www.devlang.com
+//**********************************************************************************************************************************
+#include "global.h"
+
+//==================================================================================================================================
+// Public Global Constant Definitions
+//==================================================================================================================================
+
+int const SYS_CLOCK_FREQ_HZ  = 80000000;
+int const SYS_CLOCK_FREQ_KHZ = SYS_CLOCK_FREQ_HZ / 1000;
+int const SYS_CLOCK_FREQ_MHZ = SYS_CLOCK_FREQ_KHZ / 1000; 
+
